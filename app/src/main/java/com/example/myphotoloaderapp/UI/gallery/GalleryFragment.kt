f972acdb50623a7bc8916182099a66883f1d806b
behnawwm <behban20@gmail.com>
2021-05-02 13:03:50 +0430

    navigation component preparation

---
+++ app/src/main/java/com/example/myphotoloaderapp/UI/gallery/GalleryFragment.kt
@@ -0,0 +1,8 @@
+package com.example.myphotoloaderapp.UI.gallery
+
+import androidx.fragment.app.Fragment
+import com.example.myphotoloaderapp.R
+
+class GalleryFragment : Fragment(R.layout.fragment_gallery) {
+
+}
