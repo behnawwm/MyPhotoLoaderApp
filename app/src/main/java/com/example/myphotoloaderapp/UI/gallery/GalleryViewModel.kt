47c013453824bced9d145caf85efab46095386b7
behnawwm <behban20@gmail.com>
2021-05-04 17:29:45 +0430

    Apk file inserted

--- app/src/main/java/com/example/myphotoloaderapp/UI/gallery/GalleryViewModel.kt
+++ app/src/main/java/com/example/myphotoloaderapp/UI/gallery/GalleryViewModel.kt
@@ -24,6 +24,6 @@ class GalleryViewModel @ViewModelInject constructor(
 
     companion object {
         private const val CURRENT_QUERY = "unique_query!"
-        private const val DEFAULT_QUERY = "cats"
+        private const val DEFAULT_QUERY = "beautiful"
     }
 }
